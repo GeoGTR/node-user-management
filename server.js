f9798873fd7a3f7466746eb86e307b9de4d50f30
Fatih Özgür <fatihozgurcmpe@gmail.com>
2023-09-11 12:42:59 +0300

    Mysql connection errors fixed

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 4000;
 
@@ -12,59 +12,111 @@ app.use(
 );
 // Enable CORS for all routes
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin (insecure, use a specific origin in production)
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow requests from any origin (insecure, use a specific origin in production)
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
 // get the client
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 // create the connection to database
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'test',
-  password: 'Admin123!'
+  host: "localhost",
+  user: "root",
+  password: "Ankasoft1!",
 });
 
+const createDatabaseQuery = "CREATE DATABASE IF NOT EXISTS test;";
+const useDatabaseQuery = "USE test;";
+const createTableQuery = `
+  CREATE TABLE IF NOT EXISTS user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+  );
+`;
 
+connection.query(createDatabaseQuery, (err) => {
+  if (err) {
+    console.error("Veritabanı oluşturma hatası:", err);
+    connection.end();
+    return;
+  }
 
-// Define a route to retrieve all users
-app.get('/all', (req, res) => {
-  // Select all users from the MySQL database
-  const query = 'SELECT * FROM users';
-  connection.query(
-    'SELECT * FROM `user`',
-    function(err, results, fields) {
-      console.log(results); // results contains rows returned by server
-      res.status(200).json(results);
+  // Oluşturulan veritabanını kullanma işlemi
+  connection.query(useDatabaseQuery, (err) => {
+    if (err) {
+      console.error("Veritabanı seçme hatası:", err);
+      connection.end();
+      return;
     }
-  ) 
-});
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html'); // Replace 'index.html' with your actual HTML file path
-});
+    // Tabloyu oluşturma işlemi
+    connection.query(createTableQuery, (err) => {
+      if (err) {
+        console.error("Tablo oluşturma hatası:", err);
+        connection.end();
+        return;
+      }
 
-app.get('/health', (req, res) => {
-  res.sendStatus(200);
-});
+      console.log("Veritabanı ve tablo başarıyla oluşturuldu.");
 
-app.listen(port, () => {
-  console.log(`app listening at http://localhost:${port}`);
-});
+      // Define a route to retrieve all users
+      app.get("/all", (req, res) => {
+        // Select all users from the MySQL database
+        const query = "SELECT * FROM user";
+        connection.query(query, function (err, results, fields) {
+          if (err) {
+            console.error("SQL Hatası:", err);
+            return res
+              .status(500)
+              .json({ error: "Veritabanından veri alırken bir hata oluştu." });
+          }
+          console.log(results); // results contains rows returned by server
+          res.status(200).json(results);
+        });
+      });
 
+      app.get("/", (req, res) => {
+        res.sendFile(__dirname + "/index.html"); // Replace 'index.html' with your actual HTML file path
+      });
 
-app.post('/add', (req, res) => {
-  const { name, surname, email, password } = req.query;
+      app.get("/health", (req, res) => {
+        res.sendStatus(200);
+      });
 
-  connection.query('INSERT INTO user (name, surname, email, password) VALUES (?,?,?,?)', [name, surname, email, password],(error, results) => {
-     if (error) return res.json({ error: error });
+      app.listen(port, () => {
+        console.log(`App http://localhost:${port} adresinde dinleniyor.`);
+      });
 
-     });
-     res.sendStatus(200);
+      app.post("/add", (req, res) => {
+        const { name, surname, email, password } = req.query;
 
- 
+        connection.query(
+          "INSERT INTO user (name, surname, email, password) VALUES (?,?,?,?)",
+          [name, surname, email, password],
+          (error, results) => {
+            if (error) {
+              console.error("SQL Hatası:", error);
+              return res
+                .status(500)
+                .json({
+                  error: "Veritabanına veri eklenirken bir hata oluştu.",
+                });
+            }
+
+            // Başarılı durumda sadece 200 status kodu gönderiyoruz.
+            res.sendStatus(200);
+          }
+        );
+      });
+    });
+  });
 });
