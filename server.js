c0b60faaf9410109ffddc085140fa3cc422fa2be
Fatih Özgür <fatihozgurcmpe@gmail.com>
2023-09-13 14:01:44 +0300

    test user added.

--- server.js
+++ server.js
@@ -41,6 +41,9 @@ const createTableQuery = `
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
   );
+
+  INSERT INTO user (name, surname, email, password) VALUES
+    ('Fatih', 'Özgür', 'fozgur@ankasoft.co', '123456')
 `;
 
 connection.query(createDatabaseQuery, (err) => {
