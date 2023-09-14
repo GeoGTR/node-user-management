f9aa8433db04b5e64b0a00ec607444bfb2f9dcc0
Fatih Özgür <fatihozgurcmpe@gmail.com>
2023-09-14 10:03:24 +0300

    test

--- dump/users.sql
+++ dump/users.sql
@@ -1,10 +1,18 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+CREATE DATABASE IF NOT EXISTS 'test';
+
+USE 'test';
+
 CREATE TABLE IF NOT EXISTS user (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     surname VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
-)
+);
+
+INSERT INTO user (name, surname, email, password) VALUES
+('Fatih', 'Özgür', 'fozgur@ankasoft.co','test');
+
