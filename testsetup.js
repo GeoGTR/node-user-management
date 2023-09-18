22e1c68efd9306e89891cc91648c53e74f6b83a2
Fatih Özgür <fatihozgurcmpe@gmail.com>
2023-09-18 17:43:22 +0300

    test added

---
+++ testsetup.js
@@ -0,0 +1,39 @@
+const {By} = require('selenium-webdriver');
+const {suite} =  require('selenium-webdriver/testing');
+const assert = require('assert');
+
+const chrome = require('selenium-webdriver/chrome')   
+const screen = {
+    width: 640,
+    height: 480
+};
+
+suite(env=>{
+    describe("Accounts listesi testleri",()=>{
+        let driver = new Builder()
+        .forBrowser('chrome')
+        .setChromeOptions(new chrome.Options().headless().windowSize(screen));
+        
+        before(async()=>{
+            driver = await env.builder().build();
+        });
+        after(async()=>{
+            await driver.quit();
+        });
+        it("Test kullanıcısı oluşturma",async()=>{
+            await driver.get("http://35.239.91.104:4000/");
+            await driver.findElement(By.id('name')).sendKeys('tsName');
+            await driver.findElement(By.id('surname')).sendKeys('tsSurname');
+            await driver.findElement(By.id('email')).sendKeys('tsEmail@tsEmail')
+            await driver.findElement(By.id('password')).sendKeys('tsPassword')
+
+            await driver.findElement(By.id('submit')).click()
+
+            let userlist = await driver.findElement(By.id('userList'));
+            let users = await userlist.findElements(By.tagName('li'));
+            let testuser = users[users.length-1];
+            
+            assert.equal(await testuser.getText(),'tsName tsSurname - tsEmail@tsEmail');
+        });
+    });
+});
