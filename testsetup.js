70e7d3a760e065ecfdd4c167826f4fbf2f43d826
Fatih Özgür <fatihozgurcmpe@gmail.com>
2023-09-19 13:38:46 +0300

    test

--- testsetup.js
+++ testsetup.js
@@ -1,34 +1,3 @@
-// const {By, Builder} = require('selenium-webdriver');
-// const {suite} =  require('selenium-webdriver/testing');
-// const assert = require('assert');
-
-// suite(env=>{
-//     describe("Accounts listesi testleri",()=>{
-//         let driver;
-        
-//         before(async()=>{
-//             driver = await new Builder().forBrowser('chrome').build();
-//         });
-//         after(async()=>{
-//             await driver.quit();
-//         });
-//         it("Test kullanıcısı oluşturma",async()=>{
-//             await driver.get("http://35.239.91.104:4000/");
-//             await driver.findElement(By.id('name')).sendKeys('tsName');
-//             await driver.findElement(By.id('surname')).sendKeys('tsSurname');
-//             await driver.findElement(By.id('email')).sendKeys('tsEmail@tsEmail')
-//             await driver.findElement(By.id('password')).sendKeys('tsPassword')
-
-//             await driver.findElement(By.id('submit')).click()
-
-//             let userlist = await driver.findElement(By.id('userList'));
-//             let users = await userlist.findElements(By.tagName('li'));
-//             let testuser = users[users.length-1];
-            
-//             assert.equal(await testuser.getText(),'tsName tsSurname - tsEmail@tsEmail');
-//         });
-//     });
-// });
 
 const { Builder, By, Key, until } = require('selenium-webdriver');
 
@@ -44,33 +13,65 @@ const driver = new Builder()
   .setChromeOptions(chromeOptions)
   .build();
 
-// Web sitesine git
-driver.get('http://35.239.91.104:4000/');
-driver.wait(until.titleIs('Add User'), 1000);
-//wait 10 sec
-driver.sleep(3000);
+// // Web sitesine git
 
-// Formu doldur
-driver.findElement(By.id('name')).sendKeys('John');
-driver.findElement(By.id('surname')).sendKeys('Doe');
-driver.findElement(By.id('email')).sendKeys('johndoe@example.com');
-driver.findElement(By.id('password')).sendKeys('secretpassword');
+// // Formu doldur
+// driver.findElement(By.id('name')).sendKeys('John');
+// driver.findElement(By.id('surname')).sendKeys('Doe');
+// driver.findElement(By.id('email')).sendKeys('johndoe@example.com');
+// driver.findElement(By.id('password')).sendKeys('secretpassword');
 
-// Formu gönder
-driver.findElement(By.id('submit')).click();
+// // Formu gönder
+// driver.findElement(By.id('submit')).click();
 
-// Listeyi kontrol et
-driver.wait(async () => {
-    const list = await driver.findElement(By.id('userList'));
-    const ulElement = await list.findElements(By.tagName('ul'));
-    const lastLiElement = await ulElement[ulElement.length - 1];
-    const text = await lastLiElement.getText();
-    return text.includes('John Doe - johndoe@example.com');
-  }, 5000).then(() => {
-    console.log('Form başarıyla gönderildi ve listeye eklendi.');
-  }).catch(error => {
-    console.error('Hata oluştu:', error);
-  }).finally(() => {
-    // WebDriver'ı kapat
-    driver.quit();
-  });
+// // Listeyi kontrol et
+// driver.wait(async () => {
+//     const list = await driver.findElement(By.id('userList'));
+//     const ulElement = await list.findElements(By.tagName('ul'));
+//     const lastLiElement = await ulElement[ulElement.length - 1];
+//     const text = await lastLiElement.getText();
+//     return text.includes('John Doe - johndoe@example.com');
+//   }, 5000).then(() => {
+//     console.log('Form başarıyla gönderildi ve listeye eklendi.');
+//   }).catch(error => {
+//     console.error('Hata oluştu:', error);
+//   }).finally(() => {
+//     // WebDriver'ı kapat
+//     driver.quit();
+//   });
+
+  (async () => {
+    try {
+      // Web sitesine git
+      await driver.get('http://35.239.91.104:4000/');
+  
+      // Formu doldur
+      await driver.findElement(By.id('name')).sendKeys('John');
+      await driver.findElement(By.id('surname')).sendKeys('Doe');
+      await driver.findElement(By.id('email')).sendKeys('johndoe@example.com');
+      await driver.findElement(By.id('password')).sendKeys('secretpassword');
+  
+      // Formu gönder
+      await driver.findElement(By.id('submit')).click();
+  
+      // Yeni sayfa yüklenmesini bekleyin
+      await driver.wait(until.elementLocated(By.id('userList')), 5000);
+  
+      // Listeyi kontrol et
+      const list = await driver.findElement(By.id('userList'));
+      const ulElement = await list.findElement(By.tagName('ul'));
+      const lastLiElement = await ulElement.findElement(By.xpath('//li[last()]'));
+      const text = await lastLiElement.getText();
+  
+      if (text.includes('John Doe - johndoe@example.com')) {
+        console.log('Form başarıyla gönderildi ve listeye eklendi.');
+      } else {
+        console.log('Form gönderimi başarısız veya liste güncellenmedi.');
+      }
+    } catch (error) {
+      console.error('Hata oluştu:', error);
+    } finally {
+      // WebDriver'ı kapat
+      await driver.quit();
+    }
+  })();
