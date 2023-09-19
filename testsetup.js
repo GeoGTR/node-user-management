fa482bf2a5350e843b7942ef4166a03e01078589
Fatih Özgür <fatihozgurcmpe@gmail.com>
2023-09-19 13:46:35 +0300

    test

--- testsetup.js
+++ testsetup.js
@@ -1,6 +1,9 @@
 
 const { Builder, By, Key, until } = require('selenium-webdriver');
 
+//random number between 1 and 1000
+const random = Math.floor(Math.random() * 1000) + 1;
+
 const chrome = require('selenium-webdriver/chrome');
 
 // Headless Chrome ayarları
@@ -13,42 +16,15 @@ const driver = new Builder()
   .setChromeOptions(chromeOptions)
   .build();
 
-// // Web sitesine git
-
-// // Formu doldur
-// driver.findElement(By.id('name')).sendKeys('John');
-// driver.findElement(By.id('surname')).sendKeys('Doe');
-// driver.findElement(By.id('email')).sendKeys('johndoe@example.com');
-// driver.findElement(By.id('password')).sendKeys('secretpassword');
-
-// // Formu gönder
-// driver.findElement(By.id('submit')).click();
-
-// // Listeyi kontrol et
-// driver.wait(async () => {
-//     const list = await driver.findElement(By.id('userList'));
-//     const ulElement = await list.findElements(By.tagName('ul'));
-//     const lastLiElement = await ulElement[ulElement.length - 1];
-//     const text = await lastLiElement.getText();
-//     return text.includes('John Doe - johndoe@example.com');
-//   }, 5000).then(() => {
-//     console.log('Form başarıyla gönderildi ve listeye eklendi.');
-//   }).catch(error => {
-//     console.error('Hata oluştu:', error);
-//   }).finally(() => {
-//     // WebDriver'ı kapat
-//     driver.quit();
-//   });
-
   (async () => {
     try {
       // Web sitesine git
       await driver.get('http://35.239.91.104:4000/');
   
       // Formu doldur
-      await driver.findElement(By.id('name')).sendKeys('John');
-      await driver.findElement(By.id('surname')).sendKeys('Doe');
-      await driver.findElement(By.id('email')).sendKeys('johndoe@example.com');
+      await driver.findElement(By.id('name')).sendKeys('John' + random);
+      await driver.findElement(By.id('surname')).sendKeys('Doe' + random);
+      await driver.findElement(By.id('email')).sendKeys('johndoe@example.com'+ random);
       await driver.findElement(By.id('password')).sendKeys('secretpassword');
   
       // Formu gönder
@@ -63,7 +39,7 @@ const driver = new Builder()
       const lastLiElement = await ulElement.findElement(By.xpath('//li[last()]'));
       const text = await lastLiElement.getText();
   
-      if (text.includes('John Doe - johndoe@example.com')) {
+      if (text.includes('John'+random+' Doe'+random+' - johndoe@example.com')) {
         console.log('Form başarıyla gönderildi ve listeye eklendi.');
       } else {
         console.log('Form gönderimi başarısız veya liste güncellenmedi.');
